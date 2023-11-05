d8b3509f27352d8a5474281aaf926749ab2d584c
manisha54 <manishachaudhary2057@gmail.com>
2023-11-05 16:15:00 +0545

    user registration done

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -6,3 +6,22 @@ const User = require('../models/User')
 const { verifyUser, verifyOwner } = require('../middleware/auth')
 
 const userController = require('../controllers/user_controller')
+
+
+
+//register
+router.post('/register', (req, res, next) => {
+    const { fName, lName, email, phoneNumber, password, role } = req.body
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            // res.json(user)
+            if (user) return res.status(400).json({ error: 'user already exist' })
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (err) return res.status(500).json({ error: err.message })
+                User.create({ fName, lName, email, phoneNumber, role, password: hash })
+                    .then((user) => {
+                        res.status(201).json(user)
+                    }).catch(next)
+            })
+        }).catch(next)
+})
