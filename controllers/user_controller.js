a61226469561e6d6bf0b8f92b5d5df6e7447c79f
manisha54 <manishachaudhary2057@gmail.com>
2023-11-10 06:49:18 +0545

    owner can able to delete user account

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,3 +6,27 @@ const getAllUsers = (req, res, next) => {
         .then(users => res.json(users))
         .catch(next)
 }
+
+
+
+
+const deleteAllUsers = (req, res, next) => {
+    User.deleteMany()
+        .then(reply => res.json(reply))
+        .catch(next)
+}
+
+
+const getUserById = (req, res, next) => {
+    User.findById(req.user.id)
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ error: 'user not found' })
+            }
+            res.json({
+                success: true,
+                data: [user]
+            })
+        })
+        .catch(next)
+}
