7566537ea3304de57a6c0ec7c521dc87dda1c70f
manisha54 <manishachaudhary2057@gmail.com>
2023-11-12 23:06:49 +0545

    only owner of venue can able to update details of venue

--- controllers/venue_controller.js
+++ controllers/venue_controller.js
@@ -33,3 +33,39 @@ const deleteAllvenues = (req, res, next) => {
         .then(reply => res.json(reply))
         .catch(next)
 }
+
+
+
+
+const getVenueById = (req, res, next) => {
+    Venue.findById(req.params.venue_id)
+        .then((venue) => {
+            if (!venue) {
+                res.status(404).json({ error: 'venue not found' })
+            }
+            res.json({
+                success: true,
+                data: [venue],
+            })
+        })
+        .catch(next)
+}
+
+const updateVenueById = (req, res, next) => {
+    Venue.findByIdAndUpdate(
+        req.params.venue_id,
+        { $set: req.body },
+        { new: true }
+    ).then(updatedVenue => res.json({
+        success: true,
+        data: updatedVenue, // Make sure this field contains the updated venue data
+      }))
+        .catch(next)
+}
+
+
+const deleteVenueById = (req, res, next) => {
+    Venue.findByIdAndDelete(req.params.venue_id)
+        .then(reply => res.status(204).end())
+        .catch(next)
+}
