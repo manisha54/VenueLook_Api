ee63b2896a24528672ee6519aba70fadc3f54deb
manisha54 <manishachaudhary2057@gmail.com>
2023-11-12 20:55:18 +0545

    only owner of venue can able to create venue details and delete it

--- controllers/venue_controller.js
+++ controllers/venue_controller.js
@@ -15,3 +15,21 @@ const getAllVenues = (req, res, next) => {
         }))
         .catch(next)
 }
+
+
+
+const createVenue = (req, res, next) => {
+    Venue.create(req.body)
+        .then((venue) => res.status(201).json({
+            success: true,
+            data: [venue],
+        }))
+        .catch(err => next(err))
+}
+
+
+const deleteAllvenues = (req, res, next) => {
+    Venue.deleteMany()
+        .then(reply => res.json(reply))
+        .catch(next)
+}
