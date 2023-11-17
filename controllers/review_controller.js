773e69d66e36074cfc43a4002fc2dde42518bfa4
manisha54 <manishachaudhary2057@gmail.com>
2023-11-17 15:21:45 +0545

    user can able to update and delete their comment

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -44,3 +44,62 @@ const createReview = (req, res, next) => {
 //         }).catch(next)
 // }
 
+
+
+//by id
+const getReviewById = (req, res, next) => {
+    Venue.findById(req.params.venue_id)
+        .then(venue => {
+            if (!venue) return res.status(404).json({ error: 'venue not found' })
+            const review = venue.reviews.id(req.params.review_id)
+            if (!review) return res.status(404).json({ error: 'review not found' })
+            res.json(review)
+        }).catch(next)
+}
+
+const updateReviewById = (req, res, next) => {
+    Venue.findById(req.params.venue_id)
+      .then((venue) => {
+        if (!venue) return res.status(404).json({ error: 'venue not found' });
+  
+        const reviewToUpdate = venue.reviews.find((r) => r.id === req.params.review_id);
+  
+        if (!reviewToUpdate) return res.status(404).json({ error: 'review not found' });
+  
+        if (reviewToUpdate.user.toString() !== req.user.id) {
+          return res.status(403).json({ error: 'You are not authorized to edit this review' });
+        }
+  
+        reviewToUpdate.text = req.body.text;
+  
+        venue.save()
+          .then(() => res.json(reviewToUpdate))
+          .catch(next);
+      })
+      .catch(next);
+  };
+  
+
+const deleteReviewById = (req, res, next) => {
+    Venue.findById(req.params.venue_id)
+        .then(venue => {
+            if (!venue) return res.status(404).json({ error: 'venue not found' })
+            venue.reviews = venue.reviews.filter((r) => {
+                return (r.id !== req.params.review_id
+                    && r.user !== req.user.id)
+            })
+            venue.save()
+                .then(venue => res.status(204).end())
+                .catch(next)
+        }).catch(next)
+}
+
+module.exports = {
+    getAllReview,
+    createReview,
+    // deleteAllReviews,
+    getReviewById,
+    updateReviewById,
+    deleteReviewById
+
+}
